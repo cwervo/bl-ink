937d545d6baf8b4a1c4dc738f92075b8bbe6e862
Glitch (bl-ink) <none>
2019-10-13 05:34:45 +0000

    Update README & element JS

--- blink-element.js
+++ blink-element.js
@@ -2,16 +2,14 @@ customElements.define('bl-ink',
 class extends HTMLElement {
     constructor() {
         super();
-        // let template = document.getElementById('mar-quee');
         let template = document.createElement('template');
-        // CSS taken from: https://stackoverflow.com/a/44805879
         template.innerHTML = `
         <style>
             .blink-element {
                 animation: blink;
-                animation-timing-function: var(--marquee-animation-timing-function, linear);
-                animation-duration: var(--marquee-animation-duration, 0.5s);
-                animation-iteration-count: var(--marquee-animation-iteration-count, infinite);
+                animation-timing-function: var(--blink-animation-timing-function, linear);
+                animation-duration: var(--blink-animation-duration, 0.5s);
+                animation-iteration-count: var(--blink-animation-iteration-count, infinite);
                 animation-direction: var(--blink-animation-direction, alternate);
             }
             @keyframes blink {
@@ -20,7 +18,7 @@ class extends HTMLElement {
             }
         </style>
         <span class="blink-element">
-            ${this.innerHTML || "EXAMPLE TEXT"}
+            ${this.innerHTML || ""}
         </span>
         `;
         let templateContent = template.content;
